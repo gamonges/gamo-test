87d32fe7cee801e982ac5579e51c9bfebc8a18fc
gamonges <0627gamonges@gmail.com>
2018-01-21 19:16:40 +0900

    map

--- gamo-test/ViewController.swift
+++ gamo-test/ViewController.swift
@@ -7,11 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    var myMapView : MKMapView!
+
+    @IBAction func mapButton(_ sender: Any) {
+        myMapView = MKMapView()
+        myMapView.frame = self.view.bounds
+        myMapView.delegate = self
+        
+        self.view.addSubview(myMapView)
+        
+        let myLat: CLLocationDegrees = 37.506804
+        let myLon: CLLocationDegrees = 139.930531
+        let myCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLat, myLon)
+        
+        let myLatDist: CLLocationDistance = 100
+        let myLonDist: CLLocationDistance = 100
+        
+        let myRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(myCoordinate, myLatDist, myLonDist);
+        
+        myMapView.setRegion(myRegion, animated: true)
+        
+        func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool){
+            print("regionDidCahngeAnimated")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
